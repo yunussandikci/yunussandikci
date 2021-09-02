8a3dbc60ef7c5a118e1b05a34f6a977f8d2eb1f9
Yunus Sandıkcı <yunussandikci@outlook.com>
2021-09-02 23:37:01 +0300

    Update bootstrap-ubuntu20.sh

--- bootstrap-ubuntu20.sh
+++ bootstrap-ubuntu20.sh
@@ -1,7 +1,7 @@
 # Main Utils
-sudo apt update
-sudo apt upgrade
-sudo apt install zsh htop nano zip unzip curl wget git jq espeak pyton3-pip vim gnome-tweak-tool 
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y install zsh htop nano zip unzip curl wget git jq espeak python3-pip vim gnome-tweak-tool 
 
 # Drivers
 ubuntu-drivers devices
@@ -21,8 +21,7 @@ sudo snap install pycharm-professional --classic
 sudo snap install goland --classic
 
 # L2TP
-sudo apt-get install network-manager-l2tp
-sudo apt-get install network-manager-l2tp-gnome
+sudo apt install -y network-manager-l2tp network-manager-l2tp-gnome
 
 # Install OhMyZsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -37,15 +36,15 @@ tar -xzvf go1.17.linux-amd64.tar.gz
 sudo mv go /usr/local/go
 rm go1.17.linux-amd64.tar.gz
 echo "GOROOT=/usr/local/go" >> .zshrc
-echo "export PATH=$PATH:$GOROOT/bin" >> .zshrc
+echo "export PATH=\$PATH:\$GOROOT/bin" >> .zshrc
 
 # Install Pip Tools
-sudo pip3 install ansible virtualenv gitman
+sudo pip install ansible virtualenv gitman
 
 # Install Python
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt update
-sudo apt install python3.6 python3.6-dev
+sudo apt install -y python3.6 python3.6-dev
 
 # Install Kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
@@ -57,14 +56,19 @@ wget https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubectx_v0.9.4_l
 wget https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubens_v0.9.4_linux_x86_64.tar.gz
 tar -xzvf kubectx_v0.9.4_linux_x86_64.tar.gz
 tar -xzvf kubens_v0.9.4_linux_x86_64.tar.gz
-mv kubectx /usr/local/bin/kubectx
-mv kubens /usr/local/bin/kubens
-rm kubectx_v0.9.4_linux_x86_64.tar.gz kubens_0.9.4_linux_x86_64.tar.gz LICENSE
+sudo mv kubectx /usr/local/bin/kubectx
+sudo mv kubens /usr/local/bin/kubens
+rm kubectx_v0.9.4_linux_x86_64.tar.gz kubens_v0.9.4_linux_x86_64.tar.gz LICENSE
+
+# Install Kubectl Aliases
+wget https://raw.githubusercontent.com/ahmetb/kubectl-aliases/master/.kubectl_aliases
+echo "[ -f ~/.kubectl_aliases ] && source ~/.kubectl_aliases" >> .zshrc
+
 
 # Install Helm
 wget https://get.helm.sh/helm-v3.6.2-linux-amd64.tar.gz
 tar -xzvf helm-v3.6.2-linux-amd64.tar.gz
-mv linux-amd64/helm /usr/local/bin/helm
+sudo mv linux-amd64/helm /usr/local/bin/helm
 rm -rf helm-v3.6.2-linux-amd64.tar.gz linux-amd64
 
 # Install Terraform
@@ -73,10 +77,6 @@ unzip terraform_1.0.4_linux_amd64.zip
 sudo mv terraform /usr/local/bin/terraform
 rm -rf terraform_1.0.4_linux_amd64.zip
 
-# Install Kubectl Aliases
-wget https://raw.githubusercontent.com/ahmetb/kubectl-aliases/master/.kubectl_aliases
-echo "[ -f ~/.kubectl_aliases ] && source ~/.kubectl_aliases" >> .zshrc
-
 # Install AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
@@ -84,12 +84,12 @@ sudo ./aws/install
 rm -rf aws awscliv2.zip
 
 # Install Docker
-sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release
+sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt -y update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
 sudo chmod 666 /var/run/docker.sock
 
