5871924d7a6a40cbdb2042fbe2689bf038a84900
Yunus Sandıkcı <yunussandikci@outlook.com>
2021-12-24 23:04:57 +0300

    Create .hyper.js

---
+++ .hyper.js
@@ -0,0 +1,15 @@
+module.exports = {
+    config: {
+        hypest: {
+            darkmode: true,
+            vibrancyLevel: 0.2,
+            accentColor: "white",
+        },
+        fontSize: 14,
+        fontFamily: "MesloLGS NF",
+        updateChannel: 'stable',
+        cursorColor: "rgba(255,255,255,0.6)",
+        padding: '14px 5px 14px 14px',
+    },
+    plugins: ["hyper-hypest"],
+};
