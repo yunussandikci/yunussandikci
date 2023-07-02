987b4b9d9ea8243f1e6fdecc5379fdc18f5897ef
Yunus Sandıkcı <yunussandikci@outlook.com>
2023-07-02 09:20:33 +0300

    Update .hyper.js

--- .hyper.js
+++ .hyper.js
@@ -9,6 +9,7 @@ module.exports = {
         fontFamily: "MesloLGS NF",
         updateChannel: 'stable',
         cursorColor: "rgba(255,255,255,0.6)",
+        scrollback: 50000,
         padding: '14px 5px 14px 14px',
     },
     plugins: ["hyper-hypest"],
